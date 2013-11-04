6512ae0174be0ffc83cb702da58e9f0e8f1b33bd
tmertensacker <tms.mertens@gmail.com>
2013-11-04 17:12:30 +0100

    listener veranderd

--- ZeppelinPi/src/Listener.java
+++ ZeppelinPi/src/Listener.java
@@ -7,9 +7,9 @@ public class Listener extends Thread
 	private ServerSocket serverSocket;
 	private Pi pi;
 	
-	public Listener(int port, Pi pi) throws IOException
+	public Listener(int port) throws IOException
 	{
-		this.pi = pi;
+		this.pi = new Pi();
 		serverSocket = new ServerSocket(port);
 		serverSocket.setSoTimeout(10000);
 	}
@@ -24,24 +24,25 @@ public class Listener extends Thread
 				String inMsg = in.readUTF();
 				OutputStream out = server.getOutputStream();
 				DataOutputStream outData = new DataOutputStream(out);
-				if(inMsg.equals("get distance")){
-					//DistanceMonitor monitor = new DistanceMonitor();
-					// Zoek de mediaan van N measurements...
-					//float distance = monitor.measureDistance();
-					float distance = pi.getHeight();
-					outData.writeUTF(Float.toString(distance));
+				if(inMsg.equals("get height")){
+					float height = pi.getHeight();
+					outData.writeUTF(Float.toString(height));
 				}
 				else if(inMsg.equals("get picture")){
-					/*Camera camera = new Camera();
-					camera.makePicture();*/
 					pi.makePicture();
 					InputStream inFile = new FileInputStream("picture.jpg");                        
 					copy(inFile, out);
 					inFile.close();
 				}
-				else if(inMsg.equals("start motor up")){
+				else if(inMsg.equals("turn right start"))
 					pi.startMotorUp();
-					//out.writeUTF("starting motor...");          		
+				else if(inMsg.equals("turn right stop"))
+					pi.startMotorUp();
+				else if(inMsg.contains("turn right")){
+					String[] split = inMsg.split("\\s+");
+					if(split.length > 2 )
+						System.out.println(split[2]);
+					pi.startMotorUp();   
 				}
 				else{
 					//out.writeUTF("error: unknown command");				
