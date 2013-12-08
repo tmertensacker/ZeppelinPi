dd806ad5c6124d6180685c2407c8b90dfd466a30
Jeroen <Jeroen@10.43.25.235>
2013-12-08 23:54:57 +0100

    camerarared

--- ZeppelinPi/src/pi/Camera.java
+++ ZeppelinPi/src/pi/Camera.java
@@ -15,12 +15,18 @@ public void makePicture() throws IOException{
 	try{
 		File file = new File("picture.jpg");
 		file.delete();
-	    Runtime.getRuntime().exec("raspistill -o picture.jpg -t 0");
-	    long prevTime = System.currentTimeMillis();
-		long currTime = System.currentTimeMillis();
-		while(currTime - prevTime > 3000){
-			currTime = System.currentTimeMillis();
+	    Process p = Runtime.getRuntime().exec("raspistill -o picture.jpg -t 0");
+	    try {
+			p.waitFor();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+//	    long prevTime = System.currentTimeMillis();
+//		long currTime = System.currentTimeMillis();
+//		while(currTime - prevTime > 3000){
+//			currTime = System.currentTimeMillis();
+//		}
 			// met -n kan je instellen dat er geen preview getoond wordt en dat er dus meteen een foto genomen wordt
 	}
 	catch(IOException ieo){
